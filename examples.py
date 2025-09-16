3555789c63ea80af1e67e82fb4e3b32d07410cad
erubinst <erubinst@oberlin.edu>
2025-09-16 12:06:57 -0400

    initial STN formulation, still need remove and undo, raises errors in propagate during inconsistencies

---
+++ examples.py
@@ -0,0 +1,27 @@
+from stn import STN
+import json
+import numpy as np
+
+file_path = "examples/test.json"
+#load json
+with open(file_path, "r") as f:
+    data = json.load(f)
+
+
+def add_task(stn, task_name, duration=0, start=0, end=np.inf):
+    print("Adding task:", task_name)
+    start_tp = f"{task_name}_start"
+    end_tp = f"{task_name}_end"
+    stn.add_timepoint(start_tp)
+    stn.add_timepoint(end_tp)
+    stn.add_constraint(start_tp, end_tp, duration, duration)
+    stn.add_constraint(stn.cz, start_tp, start)
+    stn.add_constraint(stn.cz, end_tp, 0, end)
+
+
+
+stn = STN()
+for task in data["tasks"]:
+    add_task(stn, task['task_name'], task['duration'], task['release_time'], task['due_time'])
+
+print(stn)
---
+++ stn.py
@@ -0,0 +1,91 @@
+import numpy as np
+import networkx as nx
+from timepoint import Timepoint
+from queue import deque
+
+
+class STN(nx.DiGraph):
+    def __init__(self):
+        super().__init__()
+        self.cz = self.add_cz_tp()
+
+    def __str__(self):
+        stn_str = "STN:\n"
+        for tp in self.nodes:
+            stn_str += f"Timepoint: {tp}\n"
+            stn_str += f"  LB: {self.nodes[tp]['data']['lb']}\n"
+            stn_str += f"  UB: {self.nodes[tp]['data']['ub']}\n"
+        return stn_str
+
+    def add_cz_tp(self):
+        "Creating the zero timepoint"
+        cz = "zero"
+        self.add_node(cz, data={'ub': 0, 'lb': 0})
+        return cz
+
+    def add_timepoint(self, tp):
+        self.add_node(tp, data = {'ub': np.inf, 'lb': 0})
+
+    def remove_timepoint(self, tp):
+        self.remove_node(tp)
+
+    def add_constraint(self, tp1, tp2, lb=0, ub=np.inf):
+        print(f"Adding constraint: {tp1} -> {tp2}, lb: {lb}, ub: {ub}")
+        self.add_edge(tp1, tp2, weight=ub, data={'new_p': True})
+        self.add_edge(tp2, tp1, weight=-lb, data={'new_p': True})
+        # propagate constraints
+        self.propagate(tp1, tp2)
+
+    def propagate(self, tp1, tp2):
+        """
+        Incremental approach to propagate constraints
+        Need to save update information to be able to undo if constraint does not work
+        """
+
+        queue = deque([tp1, tp2])
+        for tp in [tp1, tp2]:
+            self.nodes[tp]['data']['ub_p'] = True
+            self.nodes[tp]['data']['lb_p'] = True
+        while queue:
+            tp = queue.popleft()
+            tp_node = self.nodes[tp]
+            if tp_node['data']['ub_p']:
+                for tp, connected_tp, data in self.out_edges(tp, data=True):
+                    connected_tp_node = self.nodes[connected_tp]
+                    new_ub = tp_node['data']['ub'] + self[tp][connected_tp]['weight']
+                    if new_ub < connected_tp_node['data']['ub'] or (connected_tp != self.cz and connected_tp_node['data'].get('pu') is None):
+                        connected_tp_node['data']['ub'] = new_ub
+                        connected_tp_node['data']['ub_p'] = True
+                        connected_tp_node['data']['pu'] = tp
+                        # if new upper bound is less than the lower bound, raise inconsistency
+                        if connected_tp_node['data']['ub'] < -connected_tp_node['data']['lb']:
+                            raise Exception("Inconsistent STN")
+                        # If revisiting edge, raise inconsistency
+                        if data.get('new_p', False) and data.get('ub_p', False):
+                            raise Exception("Inconsistent STN")
+                        data['ub_p'] = True
+                        if connected_tp not in queue:
+                            queue.append(connected_tp)                        
+
+            if tp_node['data']['lb_p']:
+                for connected_tp, tp, data in self.in_edges(tp, data=True):
+                    connected_tp_node = self.nodes[connected_tp]
+                    new_lb = tp_node['data']['lb'] + self[connected_tp][tp]['weight']
+                    if new_lb < connected_tp_node['data']['lb'] or (connected_tp != self.cz and connected_tp_node['data'].get('pl') is None):
+                        connected_tp_node['data']['lb'] = new_lb
+                        connected_tp_node['data']['lb_p'] = True
+                        connected_tp_node['data']['pl'] = tp
+                        # if upper bound is less than the new lower bound, raise inconsistency
+                        if connected_tp_node['data']['ub'] < -connected_tp_node['data']['lb']:
+                            raise Exception("Inconsistent STN")
+                        # If revisiting edge, raise inconsistency
+                        if data.get('new_p', False) and data.get('lb_p', False):
+                            raise Exception("Inconsistent STN")
+                        data['lb_p'] = True
+                        if connected_tp not in queue:
+                            queue.append(connected_tp)                        
+            tp_node['data']['ub_p'] = False
+            tp_node['data']['lb_p'] = False
+
+
+
