12f65c6880b66f41281c6f18ed1ee3be089dac00
Esme Rubinstein <erubinst@andrew.cmu.edu>
2026-01-12 11:53:34 -0500

    commit test

--- tds/utils.py
+++ tds/utils.py
@@ -2,6 +2,7 @@ from datetime import datetime
 import pandas as pd
 import plotly.express as px
 
+
 def minutes_since_cz(timestamp_str, cz_datetime):
     """Convert ISO datetime string to minutes since cz."""
     dt = datetime.fromisoformat(timestamp_str)
